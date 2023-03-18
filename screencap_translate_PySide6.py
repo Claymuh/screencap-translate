fb56a1510921f4a01deed55d5accacb1854e1ba1
Dominik Baumann <Claymuh@cetero.net>
2023-03-18 14:13:01 +0100

    remove deprecated method call. Some cleanup

--- screencap_translate_PySide6.py
+++ screencap_translate_PySide6.py
@@ -13,23 +13,26 @@ class MainWindow(QMainWindow):
         self.central_widget = QWidget()
         self.setCentralWidget(self.central_widget)
 
+        # Set up the menu bar
         self.menu_bar = QMenuBar()
         self.setMenuBar(self.menu_bar)
 
         self.file_menu = self.menu_bar.addMenu("File")
 
+        # Quit menu entry
         self.quit_action = QAction("Quit", self)
         self.quit_action.setShortcut("Ctrl+Q")
         self.quit_action.triggered.connect(self.close)
         self.file_menu.addAction(self.quit_action)
 
+        # Open image menu entry
         self.open_image_action = QAction("Open Image", self)
         self.open_image_action.setShortcut("Ctrl+O")
         self.open_image_action.triggered.connect(self.open_image)
         self.file_menu.addAction(self.open_image_action)
 
+        # Set up image display widget
         self.image = QPixmap()
-
         self.scene = QGraphicsScene(self)
         self.scene.setSceneRect(QRectF(self.image.rect()))
 
@@ -39,6 +42,7 @@ class MainWindow(QMainWindow):
         self.graphics_view.setRenderHint(QPainter.Antialiasing)
         self.graphics_view.setScene(self.scene)
 
+        # Set up zoom slider
         self.zoom_slider = QSlider(Qt.Horizontal)
         self.zoom_slider.setMinimum(10)
         self.zoom_slider.setMaximum(200)
@@ -48,12 +52,12 @@ class MainWindow(QMainWindow):
 
         self.zoom_slider.valueChanged.connect(self.set_zoom)
 
+        # Layout of the image view
         layout = QVBoxLayout()
         layout.addWidget(self.graphics_view)
         layout.addWidget(self.zoom_slider)
         self.central_widget.setLayout(layout)
 
-        self.selection_item = None
 
     def set_zoom(self, value):
         factor = value / 100
@@ -79,14 +83,14 @@ class CustomGraphicsView(QGraphicsView):
         self.selection_rect = None
 
     def mousePressEvent(self, event):
-        self.selection_start = self.mapToScene(event.pos())
+        self.selection_start = self.mapToScene(event.position().toPoint())
 
     def mouseMoveEvent(self, event):
         if event.buttons() == Qt.LeftButton:
-            current_pos = self.mapToScene(event.pos())
+            current_pos = self.mapToScene(event.position().toPoint())
             self.selection_rect = QRectF(self.selection_start, current_pos).normalized()
             self.scene().update()
-    
+
     def mouseReleaseEvent(self, event):
         if self.selection_rect is not None:
             if hasattr(self, 'selection_item') and self.selection_item is not None:
@@ -94,10 +98,6 @@ class CustomGraphicsView(QGraphicsView):
             pen = QPen(Qt.red, 2)
             self.selection_item = self.scene().addRect(self.selection_rect, pen)
             self.selection_rect = None
-        else:
-            item = self.scene().itemAt(event.scenePos(), QTransform())
-            if item is self.image_item:
-                self.scene().clearSelection()
 
     def drawForeground(self, painter, rect):
         if self.selection_rect is not None:
